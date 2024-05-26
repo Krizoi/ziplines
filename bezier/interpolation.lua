ac5a1b26a800f70cda4be737f767c590709fb2c7
Krizzlord <137008522+Krizoi@users.noreply.github.com>
2024-05-25 22:09:05 -0700

    support for cubic easing

--- bezier/interpolation.lua
+++ bezier/interpolation.lua
@@ -1,54 +1,82 @@
 local interpolation = {}
-local a, b, c, d, w, v, l, n
+local a, b, c, d, f, w, v, l, n
 
-lerp = function(a, b, t)	-- custom lerp for formula
+lerp = function(a, b, t) -- custom lerp function
 	return (1 - t) * a + t * b
 end
 
-interpolation.bezier = function(p) -- uses a table of points to determine to use the quad or cubic curves, then make the curves
+easing = function(i, j, k, t) -- easing for cubic 
+	return lerp(lerp(i, j, t), lerp(j, k, t), t)
+end
+
+interpolation.bezier = function(p, s, f) -- making curves
 	for i, v in pairs(p) do
-    -- giving variables the values
 		if i == 1 then a = v continue end		
 		if i == 2 then b = v continue end
-
-		if i == 3 then
-			c = v
-			if i ~= 4 then
-				l = nil -- reset
-				for t = 0, 1.01, .01 do -- iteration, included 1.01 to be more accurate when the curves finish
-					n = Instance.new("Part")
-					w = lerp(lerp(a, b, t), lerp(b, c, t), t) -- formula
-          
-          -- properties
-					n.Anchored = true
-					n.CanCollide = false
-					n.TopSurface = Enum.SurfaceType.SmoothNoOutlines
-					n.Color = Color3.fromRGB(0,0,0)
-					
-					if l then -- if there was a position before after the first iteration
-						local d = (w - l)
-						local cf = CFrame.new(l, w)
-								
-						n.Position = w
-						n.Size = Vector3.new(.1, .1, d.Magnitude)
-						n.CFrame = cf * CFrame.new(0, 0, -d.Magnitude / 2) -- curving
-						n.Parent = workspace.curve
-					end
-					
-					l = w -- equals formula
-					task.wait()
+		
+		if s == "quad" then -- depends on the style
+			if i == 3 then c = v end	
+		elseif s == "cubic" then
+			if i == 3 then c = v continue end
+			if i == 4 then d = v end	
+		end
+		
+		if s == "quad" then -- styles
+			l = nil	-- resets	
+			for t = 0, 1.01, .01 do
+				n = Instance.new("Part")
+				w = lerp(lerp(a, b, t), lerp(b, c, t), t) -- formula
+				
+				n.Anchored = true
+				n.CanCollide = false
+				n.TopSurface = Enum.SurfaceType.SmoothNoOutlines
+				n.Color = Color3.fromRGB(0,0,0)
+				
+				if l then -- if a position exists
+					local s = (w - l)
+					local cf = CFrame.new(l, w)
+							
+					n.Position = w
+					n.Size = Vector3.new(.1, .1, s.Magnitude)
+					n.CFrame = cf * CFrame.new(0, 0, -s.Magnitude / 2)
+					n.Parent = f
 				end
+				
+				l = w -- set formula
+				task.wait()
 			end
-			
-			if i == 4 then
-				-- haven't made it yet
+		end
+		
+		if s == "cubic" then -- style
+			l = nil -- resets
+			for t = 0, 1.01, .01 do
+				n = Instance.new("Part")
+				w = lerp(easing(a, b, c, t), easing(b, c, d, t), t) -- formula
+
+				n.Anchored = true
+				n.CanCollide = false
+				n.TopSurface = Enum.SurfaceType.SmoothNoOutlines
+				n.Color = Color3.fromRGB(0,0,0)
+
+				if l then -- if a position exists
+					local s = (w - l)
+					local cf = CFrame.new(l, w)
+
+					n.Position = w
+					n.Size = Vector3.new(.1, .1, s.Magnitude)
+					n.CFrame = cf * CFrame.new(0, 0, -s.Magnitude / 2)
+					n.Parent = f
+				end
+
+				l = w -- set formula
+				task.wait()
 			end
 		end
 	end	
 end
 
-interpolation.clear_bezier = function() -- clears the curves in the folder, making a new proper variable soon
-  workspace.curve:ClearAllChildren()
+interpolation.clear_bezier = function(f) -- clears all parts in a specific folder
+	f:ClearAllChildren()
 end
 
 return interpolation
