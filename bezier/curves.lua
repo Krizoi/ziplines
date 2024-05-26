9d9d1336620a4865bead15fa2f5b47b1f5a5e6d6
Krizzlord <137008522+Krizoi@users.noreply.github.com>
2024-05-25 22:11:47 -0700

    customizable function

--- bezier/curves.lua
+++ bezier/curves.lua
@@ -1,21 +1,33 @@
 -- # variables
 local i = require(script.interpolation)
+local z = require(script)
 local p = workspace.points
+
+local curve = workspace.curve
+local cubic = p.cubic
+local quad = p.quad
 
 local a, b, c, d
 
--- # functions
-local generate = function() -- generate and setting up variables
-	a = p.a.a.Position
-	b = p.b.Position
-	c = p.c.c.Position
-	d = p.d.Position -- in case you want another point to modify
+local generate = function(t, f) -- generate curves
+	if t == "quad" then 
+		a = quad.a.a.Position
+		b = quad.b.Position
+		c = quad.c.c.Position
+		
+		i.bezier({a, b, c}, "quad", f)
+	end	
 	
-	i.bezier({a, b, c})
+	if t == "cubic" then
+		a = cubic.a.a.Position
+		b = cubic.b.Position
+		c = cubic.c.Position
+		d = cubic.d.d.Position
+		
+		i.bezier({a, b, c, d}, "cubic", f)
+	end	
 end
 
-while true do -- loop: generate curves, wait, remove curves
-	generate()
-	task.wait(7)
-	i.clear_bezier()
-end
+-- run that function
+generate("cubic", curve.zip_1)
+generate("quad", curve.zip_2)
