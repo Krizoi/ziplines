0a40be67aeec0cf2e0bce4bb986eeb1cb7c85891
Krizzlord <137008522+Krizoi@users.noreply.github.com>
2024-05-30 14:56:14 -0700

    added some optimization and changed into module

--- bezier/curves.lua
+++ bezier/curves.lua
@@ -1,33 +1,86 @@
 -- # variables
+local curves = { }
 local i = require(script.interpolation)
-local z = require(script)
-local p = workspace.points
-
-local curve = workspace.curve
-local cubic = p.cubic
-local quad = p.quad
-
-local a, b, c, d
-
-local generate = function(t, f) -- generate curves
-	if t == "quad" then 
-		a = quad.a.a.Position
-		b = quad.b.Position
-		c = quad.c.c.Position
-		
-		i.bezier({a, b, c}, "quad", f)
+-- # functions
+curves.generate = function(t, f, s, p)
+	local x, y, n
+	x = nil
+	y = nil
+	
+	local pp = Instance.new("ProximityPrompt")
+	pp.ActionText = "Ride"
+	pp.ObjectText = "Zipline"
+	pp.RequiresLineOfSight = false
+	
+	local anim = Instance.new("Animation")
+	anim.AnimationId = "http://www.roblox.com/asset/?id=17652401202"
+	
+	local clone = s:Clone()
+	clone.Parent = p
+	
+	local track = nil
+	
+	local m = 1.25
+	local points = { }
+	
+	local a, b, c, d
+	
+	if t == "quad" then
+		points = {clone.a.a.Position, clone.b.Position, clone.c.c.Position}
+		pp.Parent = clone.a.a
+	elseif t == "cubic" then
+		points = {clone.a.a.Position, clone.b.Position, clone.c.Position, clone.d.d.Position}
+		pp.Parent = clone.a.a
 	end	
 	
-	if t == "cubic" then
-		a = cubic.a.a.Position
-		b = cubic.b.Position
-		c = cubic.c.Position
-		d = cubic.d.d.Position
+	i.bezier(points, t, f)
+	s:Destroy()
+	
+	pp.Triggered:Connect(function(plr)
+		pp.Enabled = false
 		
-		i.bezier({a, b, c, d}, "cubic", f)
-	end	
+		local h = plr.Character:WaitForChild("Humanoid")
+		local r = plr.Character:WaitForChild("HumanoidRootPart")
+		
+		track = h.Animator:LoadAnimation(anim)
+		h.AutoRotate = false
+		r.Anchored = true
+		
+		local zo = Vector3.new(0, r.Size.Y * m, 0)
+		
+		track:Play()
+		
+		local alpha =  .01
+		
+		local parts = f:GetChildren()
+		for i = 1, #parts do
+			x = parts[i]
+			y = parts[i + 1]
+
+			if y == nil then break end
+
+			local pos_x = x.Position
+			local pos_y = y.Position
+		
+			local lerp = pos_x:Lerp(pos_y, alpha) - zo
+			local look = pos_y - zo
+		
+			local new = CFrame.new(lerp, look)
+			r.CFrame = new
+			
+			game:GetService("RunService").Heartbeat:Wait()
+			task.wait(0.03)
+			
+		end
+		
+		local final = parts[#parts].Position - zo
+		r.CFrame = CFrame.new(final, final + Vector3.new(0, 0, -1))
+		
+		r.Anchored = false
+		h.AutoRotate = true
+		track:Stop()
+		pp.Enabled = true
+	end)	
 end
 
--- run that function
-generate("cubic", curve.zip_1)
-generate("quad", curve.zip_2)
+return curves
