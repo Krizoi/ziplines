b853e93daf72b474ee777479998e5aff0e9ac45d
Krizzlord <137008522+Krizoi@users.noreply.github.com>
2024-05-30 14:56:56 -0700

    changed code snippet order to prevent it from overlapping

--- bezier/interpolation.lua
+++ bezier/interpolation.lua
@@ -1,38 +1,41 @@
 local interpolation = {}
-local a, b, c, d, f, w, v, l, n
 
-lerp = function(a, b, t) -- custom lerp function
+lerp = function(a, b, t)	
 	return (1 - t) * a + t * b
 end
 
-easing = function(i, j, k, t) -- easing for cubic 
+easing = function(i, j, k, t)
 	return lerp(lerp(i, j, t), lerp(j, k, t), t)
 end
 
-interpolation.bezier = function(p, s, f) -- making curves
+interpolation.bezier = function(p, s, f)
+	local a, b, c, d, v, x
 	for i, v in pairs(p) do
+		x = 0
 		if i == 1 then a = v continue end		
 		if i == 2 then b = v continue end
 		
-		if s == "quad" then -- depends on the style
+		if s == "quad" then
 			if i == 3 then c = v end	
 		elseif s == "cubic" then
 			if i == 3 then c = v continue end
 			if i == 4 then d = v end	
 		end
 		
-		if s == "quad" then -- styles
-			l = nil	-- resets	
-			for t = 0, 1.01, .01 do
-				n = Instance.new("Part")
-				w = lerp(lerp(a, b, t), lerp(b, c, t), t) -- formula
+		if s == "quad" then
+			local l = nil		
+			for t = 0, 1.02, .02 do
+				local n = Instance.new("Part")
+				local w = easing(a, b, c, t)
+				
 				
 				n.Anchored = true
 				n.CanCollide = false
 				n.TopSurface = Enum.SurfaceType.SmoothNoOutlines
 				n.Color = Color3.fromRGB(0,0,0)
+				n.Name = x + 1
 				
-				if l then -- if a position exists
+				if l then
 					local s = (w - l)
 					local cf = CFrame.new(l, w)
 							
@@ -42,23 +45,25 @@ interpolation.bezier = function(p, s, f) -- making curves
 					n.Parent = f
 				end
 				
-				l = w -- set formula
+				l = w
 				task.wait()
 			end
+
 		end
 		
-		if s == "cubic" then -- style
-			l = nil -- resets
-			for t = 0, 1.01, .01 do
-				n = Instance.new("Part")
-				w = lerp(easing(a, b, c, t), easing(b, c, d, t), t) -- formula
+		if s == "cubic" then
+			local l = nil
+			for t = 0, 1.02, .02 do
+				local n = Instance.new("Part")
+				local w = lerp(easing(a, b, c, t), easing(b, c, d, t), t)
 
 				n.Anchored = true
 				n.CanCollide = false
 				n.TopSurface = Enum.SurfaceType.SmoothNoOutlines
 				n.Color = Color3.fromRGB(0,0,0)
+				n.Name = x + 1
 
-				if l then -- if a position exists
+				if l then
 					local s = (w - l)
 					local cf = CFrame.new(l, w)
 
@@ -68,15 +73,26 @@ interpolation.bezier = function(p, s, f) -- making curves
 					n.Parent = f
 				end
 
-				l = w -- set formula
+				l = w
 				task.wait()
 			end
 		end
+		
+		for i, v in ipairs(f:GetChildren()) do
+			v.Name = tostring(i)
+		end
+		
 	end	
 end
 
-interpolation.clear_bezier = function(f) -- clears all parts in a specific folder
-	f:ClearAllChildren()
+interpolation.clear_bezier = function(but_ignore)
+	for i, v in pairs(workspace.curve:GetChildren()) do
+		if but_ignore and v:IsA("Folder") and v.Name == tostring(but_ignore) then
+			continue
+		else
+			v:ClearAllChildren()
+		end
+	end
 end
 
 return interpolation
