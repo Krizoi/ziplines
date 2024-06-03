6f9af4859d9e726f7bbb4cfc38cf243be6d73ee6
Krizzlord <137008522+Krizoi@users.noreply.github.com>
2024-06-02 22:29:21 -0700

    hugely changed the function to be more automatic

--- bezier/framework.lua
+++ bezier/framework.lua
@@ -1,14 +1,29 @@
+--[[
+
+THIS IS AN EXAMPLE SCRIPT
+
+]]
+
 -- # variables
 local curves = require(script.curves)
 local p = workspace.points
-local curve = workspace.curve
+
+local x = 0
+local id = tostring(17652401202)
+local m = 1.05
 
 -- # functions
 local setup = function()
-	coroutine.wrap(function() curves.generate(curve.zip_1, p.cubic_1) end)()
-	coroutine.wrap(function() curves.generate(curve.zip_2, p.quad_1) end)()
-	coroutine.wrap(function() curves.generate(curve.zip_3, p.cubic_2) end)()
-	coroutine.wrap(function() curves.generate(curve.zip_4, p.quad_2) end)()
+	local func = curves:new()
+	func.id = id
+	func.m = m
+	
+	coroutine.wrap(function() func:generate(p.cubic_1) end)()
+	coroutine.wrap(function() func:generate(p.quad_1) end)()
+	coroutine.wrap(function() func:generate(p.cubic_2) end)()
+	coroutine.wrap(function() func:generate(p.quad_2) end)()
+	coroutine.wrap(function() func:generate(p.cubic_3) end)()
+	coroutine.wrap(function() func:generate(p.quad_3) end)()
 end
 
 -- # setup
